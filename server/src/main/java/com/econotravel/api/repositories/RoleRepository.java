a49d84967e67b88580d3645242c6bbee3e4565ac
GraciaLazaro <gracia2092@gmail.com>
2022-03-25 12:10:05 +0100

    añadiendo seguridad classes packeges srois

---
+++ server/src/main/java/com/econotravel/api/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.econotravel.api.repositories;
+
+import net.jsrois.bookshelf.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role.RoleName roleName);
+}
+
---
+++ server/src/main/java/com/econotravel/api/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.econotravel.api.repositories;
+
+import net.jsrois.bookshelf.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
